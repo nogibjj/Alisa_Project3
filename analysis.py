24550e7fa5db7bc5d4e41c909c1b25557e3ca00e
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-11-07 02:58:43 +0000

    add

---
+++ analysis.py
@@ -0,0 +1,9 @@
+import csv
+import sqlite3
+import requests
+
+connects = sqlite3.connect('Analysis.db')
+cursor = connects.cursor()
+
+df=requests.get("https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-outcomes-and/download?datasetVersionNumber=1.csv")
+print(type(df))
