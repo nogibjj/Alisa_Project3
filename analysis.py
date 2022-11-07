317eb2495b6c180a363a1824829a269f520c5b50
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-11-07 14:40:01 +0000

    updated

--- analysis.py
+++ analysis.py
@@ -1,11 +1,16 @@
 import csv
 import sqlite3
 from sqlite3 import Error
-import pandas as pandas
+import pandas as pd
 
 
 connects = sqlite3.connect('Analysis.db')
 cursor = connects.cursor()
+
+
+# upload dataset
+df=pd.read_csv("aac_shelter_outcomes.csv")
+
 
 #create the table
 table = 'CREATE TABLE ANALYSIS ()'
